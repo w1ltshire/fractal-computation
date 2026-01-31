e478bea48b88d0cf00c77c81882809bee8eeadbd
wiltshire <wiltshire@ruf.st>
2026-01-31 13:55:21 +0200

    optimize

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,26 +1,43 @@
+use std::sync::{Arc, Mutex};
 use crate::Complex;
+use rayon::prelude::*;
 
-pub fn mandelbrot_color(c: &Complex, upper: u32) -> u32 {
-	let mut z = Complex::ZERO;
-	for i in 0..=upper {
-		z = z * z + c;
-		if z.norm_sqr() > 4.0 {
-			return i
-		}
+fn mandelbrot_color(cx: f64, cy: f64, max_iterations: usize) -> usize {
+	let mut zx = 0.0; // re
+	let mut zy = 0.0; // im
+	let mut x_sqr = 0.0;
+	let mut y_sqr = 0.0;
+	let mut i = 0;
+
+	while x_sqr + y_sqr < 4.0 && i < max_iterations {
+		zy = 2.0 * zy * zx + cy;
+		zx = x_sqr - y_sqr + cx;
+		x_sqr = zx * zx;
+		y_sqr = zy * zy;
+		i += 1;
 	}
-	upper
+
+	i
 }
 
 pub fn mandelbrot(data: &mut [[u8; 4]], from: &Complex, to: &Complex, width: u32, height: u32) {
-	for y in 0..height {
-		for x in 0..width {
+	let height = height as usize;
+	let width = width as usize;
+
+	let data = Arc::new(Mutex::new(data)); // i think this may slow down the computation a lil bit?
+	// but yeah it's still faster than single-threaded
+
+	(0..height).into_par_iter().for_each(|x| {
+		for y in 0..width {
 			let c = Complex::new(
-				from.re + (to.re - from.re) * x as f64 / width as f64,
-				from.im + (to.im - from.im) * y as f64 / height as f64,
+				from.re + (to.re - from.re) * y as f64 / width as f64,
+				from.im + (to.im - from.im) * x as f64 / height as f64,
 			);
-			let iter = mandelbrot_color(&c, 255);
-			let index = (y * width + x) as usize;
-			data[index] = [ 255 - iter as u8, 255 - iter as u8, 255 - iter as u8, 255];
+			let iter = mandelbrot_color(c.re, c.im, 255);
+			let index = x * width + y;
+
+			let mut data_ref = data.lock().unwrap();
+			data_ref[index] = [255 - iter as u8, 255 - iter as u8, 255 - iter as u8, 255];
 		}
-	}
+	});
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
-use macroquad::miniquad::window;
-use macroquad::prelude::{draw_texture_ex, next_frame, DrawTextureParams, Image, Texture2D, Vec2, BLACK, WHITE};
+use image::{ImageBuffer, Rgba};
 
 type Complex = num_complex::Complex<f64>;
 
 mod cpu;
 
-#[macroquad::main("Mandelbrot")]
-async fn main() {
-    let (width, height) = window::screen_size();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    const WIDTH: u32 = 3840;
+    const HEIGHT: u32 = 2160;
 
-    let from = Complex::new(-1.7, -1.3);
-    let to = Complex::new(1.0, 1.3);
+    let from = Complex::new(-2.0, -1.5);
+    let to = Complex::new(1.0, 1.5);
 
-    let mut image = Image::gen_image_color(width as u16, height as u16, BLACK);
-    let bmp = Texture2D::from_image(&image);
-    cpu::mandelbrot(image.get_image_data_mut(), &from, &to, width as u32, height as u32);
-    bmp.update(&image);
-    loop {
-        let params = DrawTextureParams {
-            dest_size: Some(Vec2::new(width, height)),
-            ..Default::default()
-        };
-        draw_texture_ex(&bmp, 0.0, 0.0, WHITE, params);
+    let mut data: Vec<[u8; 4]> = vec![[0; 4]; (WIDTH * HEIGHT) as usize];
 
-        next_frame().await
+    cpu::mandelbrot(&mut data, &from, &to, WIDTH, HEIGHT);
+
+    let mut imgbuf = ImageBuffer::new(WIDTH, HEIGHT);
+
+    for (i, pixel) in data.into_iter().enumerate() {
+        let x = (i as u32) % WIDTH;
+        let y = (i as u32) / WIDTH;
+        imgbuf.put_pixel(x, y, Rgba([pixel[0], pixel[1], pixel[2], pixel[3]]));
     }
+
+    imgbuf.save("mandelbrot.png").unwrap();
+    Ok(())
 }
